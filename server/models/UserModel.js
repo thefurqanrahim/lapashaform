4e6fd2b9eb1d823c0b8a23d0ff5fb370a130c11a
Furquan Rahim <80611141+thefurqanrahim@users.noreply.github.com>
2023-12-13 15:36:41 +0500

    Update UserModel.js

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+// user.model.js
+const mongoose = require('mongoose');
 
-const User = new mongoose.Schema(
-	{
-		name: { type: String, required: true },
-		email: { type: String, required: true, unique: true },
-		password: { type: String, required: true },
-		quote: { type: String },
-	},	
-	{ collection: 'user-data' }
-)
+const UserSchema = new mongoose.Schema(
+  {
+    authName: { type: String, required: true },
+    authEmail: { type: String, required: true },
+    authPassword: { type: String, required: true },
+  },
+  { collection: 'users' }
+);
 
-const model = mongoose.model('UserData', User)
+const User = mongoose.model('User', UserSchema);
 
-module.exports = model
+module.exports = User;
